15045ac06d071c538a6e247ebea13fc329de63ff
thisisnhip <nhinguyenphuong2005@gmail.com>
2025-05-17 20:43:30 +0700

    Update frmKhachHangCongNo.cs
    add sQuery1

--- frmQuanLyLIAM/frmKhachHangCongNo.cs
+++ frmQuanLyLIAM/frmKhachHangCongNo.cs
@@ -10,6 +10,7 @@ namespace frmQuanLyLIAM
         private void Form1_Load(object sender, EventArgs e)
         {
             string sQuery = "SELECT * FROM KhachHang";
+            string sQuery1 = "SELECT * FROM QuanLyNo";
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
